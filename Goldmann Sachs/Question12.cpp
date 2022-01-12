2da916b68b3842291c2f70f1de7d579b37ee7cfe
madhurapardeshi25 <60250002+madhurapardeshi25@users.noreply.github.com>
2022-01-12 18:59:46 +0530

    Create Question12.cpp

---
+++ Goldmann Sachs/Question12.cpp
@@ -0,0 +1,25 @@
+Q12: Find total number of squares in an NN chessboard
+
+#include <bits/stdc++.h>
+
+using namespace std;
+
+    long long squaresInChessBoard(long long N) {
+        // code here
+        long long res;
+        
+        res = N*(N+1)*(2*N+1)/6;
+        
+        return res;
+    }
+
+    int main(){
+        long long n;
+        cin>>n;
+
+        long long ans = squaresInChessBoard(n);
+
+        cout<<ans<<endl;
+
+        return 0;
+    }
