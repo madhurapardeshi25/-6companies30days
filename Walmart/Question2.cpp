14f0cb7aa79b30d2d0e3dfee9466525eb7c9274c
madhurapardeshi25 <60250002+madhurapardeshi25@users.noreply.github.com>
2022-02-02 15:13:20 +0530

    Create Question2.cpp

---
+++ Walmart/Question2.cpp
@@ -0,0 +1,22 @@
+2. Stone Game
+
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    bool stoneGame(vector<int> &piles)
+    {
+        return true;
+    }
+};
+
+int main()
+{
+    Solution s;
+    vector<int> v = {1, 2, 3, 4, 5};
+    cout << s.stoneGame(v) << endl;
+    return 0;
+}
